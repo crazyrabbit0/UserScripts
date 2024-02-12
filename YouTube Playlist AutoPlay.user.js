8dcc6fc53f6c0d7a0221972da43124768443b5b7
CrazyRabbit <crazyrabbit0@outlook.com>
2024-02-12 14:48:19 +0200

    Possible Video Skip Fix

--- YouTube Playlist AutoPlay.user.js
+++ YouTube Playlist AutoPlay.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name			YouTube Playlist AutoPlay
+// @name		YouTube Playlist AutoPlay
 // @namespace		https://github.com/crazyrabbit0
-// @version			1.1.0
+// @version		1.1.1.test
 // @description		AutoPlay next Playlist item in YouTube
-// @author			CrazyRabbit
-// @match			https://www.youtube.com/watch?v=*&list=*
-// @icon			https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @grant			none
-// @license			GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
+// @author		CrazyRabbit
+// @match		https://www.youtube.com/watch?v=*&list=*
+// @icon		https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant		none
+// @license		GPL-3.0-or-later; http://www.gnu.org/licenses/gpl-3.0.txt
 // @copyright		2023+, CrazyRabbit (https://github.com/crazyrabbit0)
 // @homepageURL		https://github.com/crazyrabbit0/UserScripts
 // @supportURL		https://github.com/crazyrabbit0/UserScripts/issues/new?assignees=crazyrabbit0&labels=help+wanted&template=&title=YouTube%20Playlist%20AutoPlay%20-%20Issue
 // @downloadURL		https://github.com/crazyrabbit0/UserScripts/raw/master/YouTube%20Playlist%20AutoPlay.user.js
 // @updateURL		https://github.com/crazyrabbit0/UserScripts/raw/master/YouTube%20Playlist%20AutoPlay.user.js
-// @run-at			document-start
+// @run-at		document-start
 // @noframes
 // ==/UserScript==
 
@@ -24,13 +24,14 @@
 
 	new MutationObserver((document_mutations, document_observer) => {
 		let play_time	= document.querySelector('span[class="ytp-time-current"]')
-		let loop		= document.querySelector('ytd-playlist-loop-button-renderer')
+		let loop	= document.querySelector('ytd-playlist-loop-button-renderer')
 		let progress	= document.querySelector('yt-page-navigation-progress')
+		let ads		= document.querySelector('.video-ads.ytp-ad-module')
 
-		if (play_time && loop && progress) {
+		if (play_time && loop && progress && !ads) {
 			new MutationObserver(play_time_mutations => {
 				for (const mutation of play_time_mutations) {
-					let has_finished	= mutation.addedNodes.length > 0 && mutation.addedNodes[0].data === document.querySelector('span[class="ytp-time-duration"]').textContent;
+					let has_finished	= play_time.textContent === document.querySelector('span[class="ytp-time-duration"]').textContent;
 					let has_next_item	= document.querySelector('ytd-playlist-panel-video-renderer[selected] + ytd-playlist-panel-video-renderer') !== null;
 					let isnt_video_loop	= loop.querySelector('path').getAttribute('d').substring(0, 3) !== 'M13';
 
